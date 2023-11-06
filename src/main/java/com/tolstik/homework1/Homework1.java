55a1a1736d1825b2f38f0a648824e85cdfcbcbdd
DmitryTolstik <149602491+DmitryTolstik@users.noreply.github.com>
2023-11-06 13:17:19 +0300

    homework 2 (#2)

--- src/main/java/com/tolstik/homework1/Homework1.java
+++ src/main/java/com/tolstik/homework1/Homework1.java
@@ -1,4 +1,6 @@
-public class Main {
+package main.java.com.tolstik.homework1;
+
+public class Homework1 {
     public static void main(String[] args) {
 
         System.out.println("Hello world! And good morning!");
---
+++ src/main/java/com/tolstik/homework2/Homework2.java
@@ -0,0 +1,15 @@
+package main.java.com.tolstik.homework2;
+
+public class Homework2 {
+    public static void main(String[] args) {
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        double m1 = Double.parseDouble(args[2]);
+        double m2 = Double.parseDouble(args[3]);
+
+        double result = 4 * (Math.pow(Math.PI, 2)) * ((Math.pow(a, 3)) / ((Math.pow(b, 2)) * (m2 + m1)));
+
+        System.out.println(result);
+    }
+}
